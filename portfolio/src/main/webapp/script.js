0a6f8f6595efec439ca78328f06442cfa69e8847
Dalen <dalen2020@gmail.com>
2020-06-01 11:49:50 -0500

    Fixes most if not all spacing consitency, moves all the stylign to the css file, and updates some class names and id names for clarity and css use.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -44,7 +44,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("thumbimgcl");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
