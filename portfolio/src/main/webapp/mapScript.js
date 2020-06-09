fabfa2d3821ca3b39af151a9879c154ebff91ec8
Dalen <dalen2020@gmail.com>
2020-06-09 21:18:33 +0000

    Adds some map pins relating to the locations in the pictures on my main page.

--- portfolio/src/main/webapp/mapScript.js
+++ portfolio/src/main/webapp/mapScript.js
@@ -1,7 +1,50 @@
 var map;
+var tokyoMarker;
+var fujiMarker;
+var prMarker;
+var nyMarker;
+var veniceMarker;
+var niagaraMarker;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 35.6762, lng: 139.6503},
-    zoom: 8
+    zoom: 4
   });
+
+  tokyoMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(35.6762, 139.6503),
+      title: "Tokyo"
+  });
+  tokyoMarker.setMap(map);
+
+  fujiMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(35.3606, 138.7274),
+      title: "Mt.Fuji"
+  });
+  fujiMarker.setMap(map);
+
+  prMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(18.2208, -66.5901),
+      title: "Puerto Rico"
+  });
+  prMarker.setMap(map);
+
+  nyMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(40.7484, -73.9857),
+      title: "Empire State Building"
+  });
+  nyMarker.setMap(map);
+
+  veniceMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(33.9850, -118.4695),
+      title: "Venice Beach"
+  });
+  veniceMarker.setMap(map);
+
+  niagaraMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(43.0962, -79.0377),
+      title: "Niagara Falls"
+  });
+  niagaraMarker.setMap(map);
 }
